eb8db97d76f942def0271efa5173f1b2fc44a526
CYJ <pknb213@naver.com>
2021-01-19 14:12:39 +0900

    Added Coroutine

--- test.py
+++ test.py
@@ -1,1 +1,21 @@
-print("Github Action Testing. . . .")
+import time
+string = "Github Action Testing. . . "
+print(string)
+
+
+def hi():
+    while True:
+        print("[ Wait ]")
+        x = (yield)
+        print(x)
+
+
+co = hi()
+next(co)
+
+co.send(string)
+time.sleep(10)
+co.send("Good Sleep")
+
+
+
