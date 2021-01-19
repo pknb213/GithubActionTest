ba483cd7a532cafe678d911c14b889a77c6dabb3
CYJ <pknb213@naver.com>
2021-01-19 14:18:55 +0900

    Updated Coroutine

--- test.py
+++ test.py
@@ -1,18 +1,21 @@
 import time
 string = "Github Action Testing. . . "
 print(string)
+a = 1
 
 
 def hi():
+    global a
     while True:
-        print("[ Wait ]")
+        print("[ Wait ] :", a)
+        a += 1
         x = (yield)
         print(x)
 
 
 co = hi()
 next(co)
-
+time.sleep(3)
 co.send(string)
 time.sleep(10)
 co.send("Good Sleep")
