3c0a53193a16e90976a4bc5ed194d1af68d6a4a0
CYJ <pknb213@naver.com>
2021-01-21 17:59:35 +0900

    Added test.py

--- test.py
+++ test.py
@@ -13,12 +13,31 @@ def hi():
         print(x)
 
 
+def b():
+    while True:
+        print("System On")
+        yield
+        return '1000'
+
+
+def c():
+    return '1000'
+
 co = hi()
+c1 = b()
+c2 = c()
+print(co, c1, c2)
 next(co)
+next(c1)
+print(c1.send('1'))
 time.sleep(3)
 co.send(string)
-time.sleep(10)
+time.sleep(5)
 co.send("Good Sleep")
+time.sleep(3)
+co.send(next(c1))
+print(next(c1))
+
 
 
 
