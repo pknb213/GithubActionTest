6e6eb403ec75f6c02d6d98105b7ea1050e458b3d
CYJ <pknb213@naver.com>
2021-01-27 13:22:55 +0900

    Added FastAPI Template

---
+++ fastapi.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+import uvicorn, requests
+from requests.auth import HTTPBasicAuth
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    print("Hello, FastaAPI Template~")
+    return {"RES": "Happy"}
+
+
+# Please, if Product Env used to execution the 'run.sh'!
+if __name__ == '__main__':
+    uvicorn.run(app, host='0.0.0.0', port=8000)
